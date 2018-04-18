78a1b3d2ba4ccc0304e66242eaf3d85e0020dd0e
Nile567 <32577913+Nile567@users.noreply.github.com>
2018-04-18 22:25:00 +0300

    Исправил 7 строчку
    возможно 5 или 6

--- 21.1.c
+++ 21.1.c
@@ -4,9 +4,10 @@
 void push(struct node* head, int data) {
 	struct node* newNode = (struct node*)malloc(sizeof(struct node));
 	newNode->data = data;
-	newNode->next = head;
+	newNode->next = NULL;
 	head = newNode;
 }
 void push_test() {
 	List head = build();
 	push(head, 1);
+}
